bd532da32dd1d16528d48b78c116112e710e372a
liang@123.com <liang@123.com>
2020-10-28 22:50:52 +0800

    第二次更新

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# myapp
+# newprogram
 
 ## Project setup
 ```
--- src/main.js
+++ src/main.js
@@ -1,11 +1,18 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import MintUI from 'mint-ui'
-import "mint-ui/lib/style.min.css"
-import axios from 'axios'
+import MintUI from "mint-ui"
+import 'mint-ui/lib/style.min.css'
+import axios from "axios"
 
 Vue.use(MintUI);
 Vue.prototype.axios=axios;
-createApp(App).use(store).use(router).mount('#app')
+axios.defaults.baseURL="https://www.bilibili.com";
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
