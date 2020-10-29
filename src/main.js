d6724759eb755012441c35b529cd790e3c0c0db3
liang@123.com <liang@123.com>
2020-10-29 09:54:30 +0800

    更新死三次

--- src/main.js
+++ src/main.js
@@ -2,13 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import MintUI from "mint-ui"
+import axios from 'axios'
+import MintUI from 'mint-ui'
 import 'mint-ui/lib/style.min.css'
-import axios from "axios"
 
 Vue.use(MintUI);
 Vue.prototype.axios=axios;
-axios.defaults.baseURL="https://www.bilibili.com";
+axios.defaults.baseURL="http://127.0.0.1:3000";
 Vue.config.productionTip = false
 
 new Vue({
